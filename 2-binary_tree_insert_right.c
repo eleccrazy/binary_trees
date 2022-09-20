202ad27347973bad6a2a08325dd4b87e4008ab3c
Gizachew Bayness <eleccrazy24@gmail.com>
2022-09-20 11:59:44 -0400

    Bug hunted

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,17 +13,13 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *newNode;
 
-	if (newNode == NULL)
-		return (NULL);
 	if (parent == NULL)
 		return (NULL);
-
-	newNode->n = value;
-	newNode->parent = parent;
-	newNode->left = NULL;
-	newNode->right = NULL;
+	newNode = binary_tree_node(parent, value);
+	if (newNode == NULL)
+		return (NULL);
 
 	if (parent->right != NULL)
 	{
