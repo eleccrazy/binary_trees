7b6067c24bdf9680faefdaee337270fa4a98ba95
Gizachew Bayness <eleccrazy24@gmail.com>
2022-09-21 12:03:33 -0400

    Adding helper function for checking null condition

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,9 +1,11 @@
 #include "binary_trees.h"
 
+int helper(const binary_tree_t *t);
+
 /**
  * binary_tree_is_full - Checks if a binary tree is full.
  *
- * @tree: A pointer to the e root node of the tree to check
+ * @tree: A pointer to the root node of the tree to check
  *
  * Return: 0 if tree is NULL or if it is not full, otherwise it
  * returns 1.
@@ -12,13 +14,27 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree != NULL)
-	{
-		if ((tree->left != NULL && tree->right == NULL) ||
-		    (tree->left == NULL && tree->right != NULL) ||
-		    binary_tree_is_full(tree->left) == 0 ||
-		    binary_tree_is_full(tree->right) == 0)
-			return (0);
-	}
+	if (tree == NULL)
+		return (0);
+	return (helper(tree));
+}
+
+/**
+ * helper - Performs the actual check.
+ *
+ * @t: A pointer to the root node of the tree to check.
+ *
+ * Return: 0 if tree is NULL or if it is not full, otherwise it returns 1.
+ *
+ */
+
+int helper(const binary_tree_t *t)
+{
+	if (t == NULL)
+		return (1);
+	if ((t->left != NULL && t->right == NULL) ||
+			(t->left == NULL && t->right != NULL) ||
+			helper(t->left) == 0 || helper(t->right) == 0)
+		return (0);
 	return (1);
 }
