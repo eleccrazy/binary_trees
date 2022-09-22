040fe1d6bfe550693fd64e73332039afc6e0b066
Gizachew Bayness <eleccrazy24@gmail.com>
2022-09-22 14:31:40 -0400

    Header file updated

--- binary_trees.h
+++ binary_trees.h
@@ -71,5 +71,6 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
 
 #endif
